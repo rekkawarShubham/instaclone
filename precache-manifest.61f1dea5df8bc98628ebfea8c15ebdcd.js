03ddcf7a211fd4fb4b70024d6797ca5d9333e3ea
Shubham Rekkawar <rekkawars111@gmail.com>
2020-07-30 23:56:29 +0530

    Updates

---
+++ precache-manifest.61f1dea5df8bc98628ebfea8c15ebdcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d7e3d69054ba0283baa17053a1b4d3",
+    "url": "/instaclone/index.html"
+  },
+  {
+    "revision": "997c1442ef1ee615ed27",
+    "url": "/instaclone/static/css/main.7e317906.chunk.css"
+  },
+  {
+    "revision": "c633e16b9beca98bfeee",
+    "url": "/instaclone/static/js/2.544f6319.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instaclone/static/js/2.544f6319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997c1442ef1ee615ed27",
+    "url": "/instaclone/static/js/main.55d032e2.chunk.js"
+  },
+  {
+    "revision": "44dee22dbf01109bc029",
+    "url": "/instaclone/static/js/runtime-main.d4e0309d.js"
+  }
+]);
